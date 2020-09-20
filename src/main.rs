2cc0a53997757ddf0c960d533ca24f173c0cdb81
Brandon Gomes <bhgomes.code@gmail.com>
2020-09-20 16:17:42 -0400

    setup package

---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("This project is currently in development.\n\rThe purpose of this crate is to reserve the package name.\n\rIf you believe that this is a mistake email Brandon H. Gomes at bhgomes@pm.me");
+}
