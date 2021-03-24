0f8efbe4d8053ac9ebae67e23e012563b7ec3dc5
Brandon Gomes <bh.gomes@rutgers.edu>
2021-03-24 18:43:07 -0400

    fix typos

--- docs/js/wasm.js
+++ docs/js/wasm.js
@@ -208,7 +208,7 @@ function addBorrowedObject(obj) {
 }
 function __wbg_adapter_16(arg0, arg1, arg2) {
     try {
-        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0aefd8175f475db4(arg0, arg1, addBorrowedObject(arg2));
+        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h04d3a30fcf624437(arg0, arg1, addBorrowedObject(arg2));
     } finally {
         heap[stack_pointer++] = undefined;
     }
